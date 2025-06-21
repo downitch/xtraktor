04b55ccbb0ffce3f6a7302e75f1a0ef49c992160
д0wnиt4 <20886900+downitch@users.noreply.github.com>
2025-06-21 11:54:51 +0200

    appreciation to other developers added

--- config.js
+++ config.js
@@ -2,19 +2,14 @@ export const OUTPUT_DIR = './output';
 
 export const UNIVERSAL_REGEX = new RegExp(
   [
-    // Full URLs
     /\b(?:https?|wss?):\/\/[^\s"'`\\<>{}]+/,
-    // Relative paths: /anything/... (exclude ${}, quotes, etc.)
     /(?<![a-zA-Z0-9])\/[a-zA-Z0-9/_\-?=&#.%]+/,
-    // fetch("...") or axios.get("...")
     /\b(?:fetch|axios\.\w+)\(['"]([^'"${}]+)['"]\)/,
-    // ("/...") relative paths in parenthesis
-    // /(^.*?("|')(\/[\w\d\W\?\/&=\#\.\!:_-]*?)(\2).*$)/,
-    /this\.fetch\(this\.url\("([^"]+)"\)/,
-    /"\?(.*?)"/,
-    /"\/(.*?)"/,
-    /'\/(.*?)'/,
-    /`\/(.*?)`/
+    /this\.fetch\(this\.url\("([^"]+)"\)/, // thank you @SirBugs
+    /"\?(.*?)"/, // thank you @SirBugs
+    /"\/(.*?)"/, // thank you @SirBugs
+    /'\/(.*?)'/, // thank you @SirBugs
+    /`\/(.*?)`/, // thank you @SirBugs
   ]
     .map(r => r.source)
     .join('|'),
